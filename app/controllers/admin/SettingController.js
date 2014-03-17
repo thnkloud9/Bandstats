c3c1637966b615165a6628773146127f06bf3bc0
Mark Lewis <thnkloud9@gmail.com>
2014-03-16 22:18:24 -0300

    fix settings controller

--- app/controllers/admin/SettingController.js
+++ app/controllers/admin/SettingController.js
@@ -33,8 +33,6 @@ SettingController.prototype.indexAction = function(req, res) {
             _.extend(data, {"error": err});
         }
         _.extend(data, { 'config': JSON.stringify(results) });
-        var template = require('./../../views/setting_index');
-        //res.send(template.render(data));
         res.send(data);
     });
 }
