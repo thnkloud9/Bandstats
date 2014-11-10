a27317866652449a1eb0f2aef48ac82e25186ffb
Mark Lewis <thnkloud9@gmail.com>
2014-11-10 01:29:06 +0100

    new bandstats utils class

---
+++ app/lib/BandstatsUtils.js
@@ -0,0 +1,37 @@
+/**
+ * Bandstats Utils
+ *
+ * author: Mark Lewis
+ */
+
+var nconf = require('nconf');
+var request = require('request');
+var async = require('async');
+var _ = require('underscore');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+/**
+ * Constructor
+ *
+ */
+function BandstatsUtils() {
+    nconf.file(path.join(__dirname, '../config/app.json'));
+};
+
+
+BandstatsUtils.prototype.sanitizeSearchString = function(text) {
+    sanitized_text = text.toLowerCase();
+    sanitized_text = sanitized_text.replace('&', 'and')
+        .replace(/[\+\*\,\.\?\!\-\;\:\'\(\)]+/g, '')
+        .replace(/[\"“\'].+[\"”\']/g, '')
+        .replace(/[\n\r]/g, '')
+        .replace(/[\[\]]/g, '')
+        .replace(/[\\\/]/g, '');
+ 
+    return sanitized_text;
+
+}
+
+module.exports = BandstatsUtils;
